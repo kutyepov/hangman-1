2afc29b202a7303fdbef8c17129c9d3933fa8a1a
Jason Paulino <jasonp0830@gmail.com>
2020-03-04 13:43:45 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,55 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function GuessAmount({ guesses = [] }) {
+  return (
+    <ul>
+      {guesses.map(({ guess_letter }) => (
+        <li>
+          {guess_letter}
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+function Hangman() {
+  return (
+    <p>Hangman</p>
+  )
+}
+
+function Answer() {
+  return (
+    <ul>
+      <li>Letter</li>
+    </ul>
+  );
+}
+
+class AnswerInput extends React.Component {
+  render() {
+    return (
+      <input />
+    )
+  }
+}
+
+class HangmanContainer extends React.Component {
+  constructor(props) {
+    this.state = {};
+  }
+
+  render() {
+    return (
+     <div>
+        <GuessAmount />
+        <Hangman />
+     </div>
+    )
+  }
+}
+
 function App() {
   return (
     <div className="App">
