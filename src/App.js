89e2f9a53125470e5a1df3c7059f36039a635d74
Jason Paulino <jasonp0830@gmail.com>
2020-05-27 18:38:17 -0400

    done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from 'react-bootstrap/Button';
+import InputGroup from 'react-bootstrap/InputGroup'
 
 function GuessAmount({ guesses = [] }) {
   return (
@@ -27,23 +29,25 @@ class Answer extends React.Component {
 
   render() {
     const { word, letter, correctlyGuessedLetters } = this.props;
-    let lines = []
+    let lines = [];
+    // let wrongLetters = this.props.wrongLetters.length;
     console.log(correctlyGuessedLetters);
     //apple
     //["a", "p", "p", "l", "e"]
     // correctlyGuessedLetters
     //['a', 'e']
-    let haveWeEverHitElseClause = false;
+    let haveWeEverHitElseClause = 0;
     for (let i = 0; i < word.length; i++) {
       if (correctlyGuessedLetters.indexOf(word[i]) != -1) {
         lines.push(<li>{word[i]}</li>);
+        ++haveWeEverHitElseClause;
       } else {
-        haveWeEverHitElseClause = true;
         lines.push(<li>_</li>);
       }
     }
 
-    if (!haveWeEverHitElseClause) {
+    console.log(haveWeEverHitElseClause);
+    if (haveWeEverHitElseClause >= word.length + 1) {
       alert('congrats');
     }
 
@@ -53,6 +57,8 @@ class Answer extends React.Component {
       </ul>
     );
   }
+
+
 }
 
 class IncorrectLetters extends React.Component {
@@ -61,22 +67,27 @@ class IncorrectLetters extends React.Component {
   };
 
   render() {
-      let word = this.props.word;
-      let correctlyGuessedLetters = this.props.correctlyGuessedLetters;
-      let wrongLetters = this.props.wrongLetters;
+    let word = this.props.word;
+    let correctlyGuessedLetters = this.props.correctlyGuessedLetters;
+    let wrongLetters = this.props.wrongLetters;
 
+    console.log(wrongLetters.length);
 
-
-      return (
-        <div>
-          <span>Wrong Letters:</span>
-          {
-            wrongLetters
-          }
-        </div>
-      )
-
+    if (wrongLetters.length > 7) {
+      alert("You Lose");
+      window.location.reload(false);
     }
+
+    return (
+      <div>
+        <span>Wrong Letters:</span>
+        {
+          wrongLetters
+        }
+      </div>
+    )
+
+  }
 
 }
 
@@ -91,13 +102,13 @@ class AnswerInput extends React.Component {
   render() {
     return (
       <div>
-        <input type="text" maxlength="1" value={this.state.inputValue} onChange={(e) => {
+        <input type="text" maxLength="1" value={this.state.inputValue} onChange={(e) => {
           this.setState({ inputValue: e.target.value })
         }} />
-        <button onClick={() => {
+        <Button onClick={() => {
           console.log(this.state.inputValue);
           this.props.onInputSubmitted(this.state.inputValue);
-        }}>Guess</button>
+        }}>Guess</Button>
       </div>
     )
   }
@@ -115,11 +126,13 @@ class HiddenInput extends React.Component {
     return (
       <div>
         <input type="password" value={this.state.inputValue} onChange={(e) => {
-          this.setState({ inputValue: e.target.value })
+          this.setState({ inputValue: e.target.value.replace(' ', '') })
         }} />
-        <button onClick={() => {
-          this.props.onInputSubmitted(this.state.inputValue);
-        }}>Start Game</button>
+        <Button onClick={() => {
+          if (this.state.inputValue.length >= 0) {
+            this.props.onInputSubmitted(this.state.inputValue);
+          }
+        }}>Start Game</Button>
       </div>
     )
   }
@@ -144,7 +157,7 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      word: '',
+      word: 'x',
       letter: '',
       correctlyGuessedLetters: [],
       wrongLetters: [],
@@ -157,12 +170,12 @@ class App extends React.Component {
       <div className="App">
         <GuessAmount />
         <Hangman hang={this.state.wrongLetters.length} />
-        <IncorrectLetters 
+        <IncorrectLetters
           correctlyGuessedLetters={this.state.correctlyGuessedLetters}
           word={this.state.word}
           wrongLetters={this.state.wrongLetters}
         />
-        <Answer 
+        <Answer
           word={this.state.word}
           letter={this.state.letter}
           correctlyGuessedLetters={this.state.correctlyGuessedLetters}
@@ -171,13 +184,13 @@ class App extends React.Component {
           const isThisCorrectLetter = this.state.word.indexOf(letter) !== -1;
           let updatedCorrectlyGuessedLetters = this.state.correctlyGuessedLetters.slice();
           let updatedIncorrectlyGuessedLetters = this.state.wrongLetters.slice();
-          
+
           if (isThisCorrectLetter) {
             updatedCorrectlyGuessedLetters.push(letter);
           } else {
             updatedIncorrectlyGuessedLetters.push(letter);
           }
-          this.setState({ 
+          this.setState({
             letter,
             correctlyGuessedLetters: updatedCorrectlyGuessedLetters,
             wrongLetters: updatedIncorrectlyGuessedLetters,
