fa0611f3d2e29840099c86fd51d2460fd556ee28
Stanislav Kutyepov <s.kutyepov@gmail.com>
2020-06-10 16:52:17 -0400

    Updates

---
+++ precache-manifest.9b8690979ea336dcb7686139fb3bbab0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccc98ed6d14f3050b15d08b7549e9bb0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7eec6106de44cad2062a",
+    "url": "/static/css/2.ace36aee.chunk.css"
+  },
+  {
+    "revision": "1378e17219f0f90111ad",
+    "url": "/static/css/main.a99c0530.chunk.css"
+  },
+  {
+    "revision": "7eec6106de44cad2062a",
+    "url": "/static/js/2.5c537cf8.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/static/js/2.5c537cf8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1378e17219f0f90111ad",
+    "url": "/static/js/main.ad65c204.chunk.js"
+  },
+  {
+    "revision": "449a3fe35244aa1f6eba",
+    "url": "/static/js/runtime-main.0bf214f5.js"
+  }
+]);
